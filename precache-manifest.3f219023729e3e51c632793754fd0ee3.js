306a673ffac4caf243eded2aaa5be0d85217d735
gdrossetto <gabriel.dadamosrossetto@gmail.com>
2020-05-24 16:37:58 -0300

    Updates

---
+++ precache-manifest.3f219023729e3e51c632793754fd0ee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e113501f89d7d69d0bdf1eb4fe22a9d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a95cb9cd98c5d782b99",
+    "url": "/static/css/main.5bc097d3.chunk.css"
+  },
+  {
+    "revision": "bd05c9fadeba7d12ac05",
+    "url": "/static/js/2.160c8c25.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/static/js/2.160c8c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a95cb9cd98c5d782b99",
+    "url": "/static/js/main.d4f2be3c.chunk.js"
+  },
+  {
+    "revision": "7e357a58dad5fc1d64a9",
+    "url": "/static/js/runtime-main.b97cbbce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
